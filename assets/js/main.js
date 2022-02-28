9ecd65b00c9be970bc8b366d0e9b5b83efcb8433
Ahmed Elgaidi <49118737+AhmedElgaidi@users.noreply.github.com>
2022-02-28 17:23:00 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+/*=============== SHOW MODAL ===============*/
+const showModal = (openButton, modalContent) =>{
+    const openBtn = document.getElementById(openButton),
+    modalContainer = document.getElementById(modalContent)
+    
+    if(openBtn && modalContainer){
+        openBtn.addEventListener('click', ()=>{
+            modalContainer.classList.add('show-modal')
+        })
+    }
+}
+showModal('open-modal','modal-container')
+
+/*=============== CLOSE MODAL ===============*/
+// we didn't use document.getElementById as we have to places in the popup to close from. now, we can loop throw.
+const closeBtn = document.querySelectorAll('.close-modal')
+
+function closeModal(){
+    const modalContainer = document.getElementById('modal-container')
+    modalContainer.classList.remove('show-modal')
+}
+closeBtn.forEach(c => c.addEventListener('click', closeModal))
